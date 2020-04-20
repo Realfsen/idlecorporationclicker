77779a97bee51ef87edbc45a79d1a765868a1e44
Toralf Tokheim <toralf@tokheim.no>
2020-04-20 07:50:47 +0200

    Adds user login and updates dependencies

--- app/src/main/java/com/example/idlecorporationclicker/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/idlecorporationclicker/ui/home/HomeViewModel.kt
@@ -3,11 +3,13 @@ package com.example.idlecorporationclicker.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeViewModel : ViewModel() {
 
 	private val _text = MutableLiveData<String>().apply {
-		value = "This is home Fragment"
+//		value = "This is home Fragment"
+		value = "UserID: " + FirebaseAuth.getInstance().uid.toString()
 	}
 	val text: LiveData<String> = _text
 }
